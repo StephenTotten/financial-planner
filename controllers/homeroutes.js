300d0ed4984e7731155f175e98263d74e40179d1
Crothos <s.g.totten@gmail.com>
2023-02-12 14:27:24 -0600

    login routing bug fixed

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -18,7 +18,7 @@ router.get('/', async (req, res) => {
     const goals = goalData.map((goal) => goal.get({ plain: true }));
 
     // Pass serialized data and session flag into template
-    res.render('homepage', { 
+    res.render('login', { 
       goals, 
       logged_in: req.session.logged_in 
     });
