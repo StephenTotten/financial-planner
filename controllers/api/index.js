ae4f1b4a2ea8ceaca97f9cfb7121d378aa24352c
Crothos <s.g.totten@gmail.com>
2023-02-13 11:50:33 -0600

    checkbook basic routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const goalRoutes = require('./goalRoutes');
+const checkbookRoutes = require('./checkbookRoutes');
 
 router.use('/users', userRoutes);
 router.use('/goals', goalRoutes);
+router.use('/checkbook', checkbookRoutes);
 
 module.exports = router;
