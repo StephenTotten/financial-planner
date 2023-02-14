26a6789737746038a1c5dc12954332f5b31f32d3
JessicaIOrtega <jes.ortega7@gmail.com>
2023-02-14 13:16:39 -0600

    pull feb14

--- config/connection.js
+++ config/connection.js
@@ -7,9 +7,9 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-    DB_NAME='finance_db',
-    DB_USER='root',
-    DB_PASSWORD='Password123',
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
       host: 'localhost',
       dialect: 'mysql',
