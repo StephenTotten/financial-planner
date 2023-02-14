f93262421560f6aa9974129145b74434bbff51c4
leyden <fusionsoccer05@gmail.com>
2023-02-14 10:38:24 -0600

    organizing script code

--- public/js/bigcharts.js
+++ public/js/bigcharts.js
@@ -1,4 +1,61 @@
-// API CALL AND APPLY DYNAMICALLY
+// HARDCODED WANTS AND NEEDS CHART
+
+  const WantsNeedsChart = document.getElementById('wnChart'); new Chart(WantsNeedsChart, {
+    type: 'doughnut',
+    data: {
+      labels: ['Needs 50%', 'Wants 30%', 'Savings 20%'],
+      datasets: [{
+        label: 'Budgeting',
+        data: [50, 30, 20],
+        backgroundColor: ['rgb(27, 154, 170)', 'rgb(239, 71, 111)', 'rgb(6, 214, 160)'],
+        hoverOffset: 4
+      }]
+    },
+    options: {
+      responsive: true,
+      tooltips: {
+        enabled: true
+      },
+      legend: {
+        position: 'top'
+      },
+      layout: {
+        padding: {
+          left: 0,
+          right: 0
+        }
+      }
+    }
+  })
+
+
+  // const BreakdownChart = document.getElementById('myBreakdownChart'); new Chart(ctx, {
+  //   type: 'doughnut',
+  //   data: {
+  //     labels: ['Categories', 'Wants 30%', 'Savings 20%'],
+  //     datasets: [{
+  //       label: 'Budgeting',
+  //       data: [50, 30, 20],
+  //       backgroundColor: ['rgb(27, 154, 170)', 'rgb(239, 71, 111)', 'rgb(6, 214, 160)'],
+  //       hoverOffset: 4
+  //     }]
+  //   },
+  //   options: {
+  //     responsive: true,
+  //     tooltips: {
+  //       enabled: true
+  //     },
+  //     legend: {
+  //       position: 'top'
+  //     },
+  //     layout: {
+  //       padding: {
+  //         left: 0,
+  //         right: 0
+  //       }
+  //     }
+  //   }
+  // });
 // var BudgetOBJ = {
 //   "Entertainment": [],
 //   "Utilities": [],
