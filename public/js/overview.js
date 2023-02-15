357c975e9d94466e3a722d46f0019568ec5f5fee
JessicaIOrtega <jes.ortega7@gmail.com>
2023-02-14 22:23:34 -0600

    added overview code

--- public/js/overview.js
+++ public/js/overview.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', async () => {
   tbody.innerHTML = html;
 });
 
-function createCells(cells){
+function createCells(cells) {
   return `<td></td>
   <td>${cells.category}</td>
   <td>${cells.description}</td>
@@ -16,7 +16,7 @@ function createCells(cells){
 
 async function newFormInput(event) {
   event.preventDefault();
-  
+
   const amount = document.querySelector('#amountCheckbook').value;
   const description = document.querySelector('#descriptionCheckbook').value;
   const category = document.querySelector('select').value;
@@ -26,14 +26,14 @@ async function newFormInput(event) {
     body: JSON.stringify({
       category,
       description,
-      'amount':+amount,
+      'amount': +amount,
     }),
     headers: {
       'Content-Type': 'application/json',
     },
   });
- 
-  if (response.ok) { 
+
+  if (response.ok) {
     const item = await response.json();
     console.log(item);
     const html = `<tr>${createCells(item)}</tr>`;
@@ -45,109 +45,3 @@ async function newFormInput(event) {
 }
 
 document.querySelector('#checkbookForm').addEventListener('submit', newFormInput);
-  
-// const addIncomeBtn = document.querySelector('#addIncome');
-// const addExpenseBtn = document.querySelector('#addExpense');
-
-// function addIncome() {
-//   const amountInput = document.querySelector('#amountCheckbook');
-//   const descriptionInput = document.querySelector('#descriptionCheckbook');
-//   const categorySelect = document.querySelector('select');
-
-//   const amount = amountInput.value;
-//   const description = descriptionInput.value;
-//   const category = categorySelect.value;
-
-//   const newIncome = {
-//     type: 'income',
-//     amount: amount,
-//     description: description,
-//     category: category
-//   };
-
-//   // Get list of incomes from localStorage
-//   // Add new income to the list
-//   // Save the updated income list to localStorage
-//   const incomeList = JSON.parse(localStorage.getItem('incomes')) || [];
-//   incomeList.push(newIncome);
-
-//   // Save updated list to localStorage
-//   localStorage.setItem('incomes', JSON.stringify(incomeList));
-// };
-
-// function addExpense() {
-//   const amountInput = document.querySelector('#amountCheckbook');
-//   const descriptionInput = document.querySelector('#descriptionCheckbook');
-//   const categorySelect = document.querySelector('select');
-
-//   const amount = amountInput.value;
-//   const description = descriptionInput.value;
-//   const category = categorySelect.value;
-
-//   const newIncome = {
-//     type: 'expense',
-//     amount: amount,
-//     description: description,
-//     category: category
-//   };
-
-
-//   const expenseList = JSON.parse(localStorage.getItem('expenses')) || [];
-//   incomeList.push(newIncome);
-
-//   localStorage.setItem('expenses', JSON.stringify(incomeList));
-// };
-
-// addIncomeBtn.addEventListener('click', addIncome);
-// addExpenseBtn.addEventListener('click', addExpense);
-
-
-
-// // Get the saved incomes and expenses from localStorage
-// const incomeList = JSON.parse(localStorage.getItem('incomes')) || [];
-// const expenseList = JSON.parse(localStorage.getItem('expenses')) || [];
-
-
-
-// document.addEventListener('DOMContentLoaded', function () {
-//   // code that needs to wait for the DOM to be loaded
-
-//   function generateTableRow(data) {
-//     return `
-// <tr>
-//   <td>${data.type}</td>
-//   <td>${data.category}</td>
-//   <td>${data.description}</td>
-//   <td>${data.amount}</td>
- 
-// </tr>
-// `;
-//   }
-
-//   // Generate HTML table for the combined table
-//   let table = `
-//   <table class="table table-hover">
-//     <thead>
-//       <tr>
-//         <th scope="col">Type</th>
-//         <th scope="col">Category</th>
-//         <th scope="col">Description</th>
-//         <th scope="col">Amount</th>
-//       </tr>
-//     </thead>
-//     <tbody>
-//       `;
-//   incomeList.forEach(income => {
-//     table += generateTableRow(income);
-//   });
-//   expenseList.forEach(expense => {
-//     table += generateTableRow(expense);
-//   });
-//   table += `
-//     </tbody>
-//   </table>
-//   `;
-
-//   document.querySelector('.table-container').innerHTML = table;
-
-// });
